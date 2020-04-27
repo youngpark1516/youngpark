bd73a07988cf6cd53c2a61a90eead205b283014c
youngpark1516 <cypark1516@gmail.com>
2020-04-27 19:26:17 +0800

    노트

--- python/taegeukgee.py
+++ python/taegeukgee.py
@@ -46,10 +46,10 @@ def dist(x, y):
 #틀
 move(-300, -200)
 square(600,400)
-move(-300, 200)
-turtle.goto(300,-200)
-move(-300,-200)
-turtle.goto(300,200)
+# move(-300, 200)
+# turtle.goto(300,-200)
+# move(-300,-200)
+# turtle.goto(300,200)
 
 #문양 layer 1
 move(0, -100)
@@ -129,25 +129,95 @@ square(101, 8.33)
 turtle.end_fill()
 
 #곤괘 layer 3
-move(120.82, -138.69)
+move(120.82, -138.64)
 turtle.begin_fill()
 square(101, 8.33)
 turtle.end_fill()
 
 #곤괘 layer 4
-turtle.pensize(8.3)
-move(300, -200)
+turtle.pensize(8.33)
+move(225, -150)
 turtle.goto(100, -66.66)
 
 home()
 
-#이괘
+#이괘 layer 1
+turtle.pensize(1)
 turtle.color("black")
 move(-155.47 , -161.79)
 turtle.left(33.69)
 turtle.begin_fill()
 square(66.66, 100)
 turtle.end_fill()
+
+#이괘 layer 2
+move(-141.61, -152.55)
+turtle.color("white")
+turtle.begin_fill()
+square(8.33, 101)
+turtle.end_fill()
+
+#이괘 layer 3
+move(-120.82, -138.69)
+turtle.begin_fill()
+square(8.33, 101)
+turtle.end_fill()
+
+#이괘 layer 4
+turtle.pensize(8.3)
+move(-225, -150)
+turtle.goto(-100, -66.66)
+
+#이괘 layer 5
+turtle.pensize(1)
+turtle.color("black")
+move(-155.47 , -161.79)
+turtle.begin_fill()
+square(15, 100)
+turtle.end_fill()
+
+#이괘 layer 6
+move(-113 , -134)
+turtle.begin_fill()
+square(16, 100)
+turtle.end_fill()
+
+home()
+
+#감괘 layer 1
+move(155.47, 41.61)
+turtle.left(33.69)
+turtle.begin_fill()
+square(66.66, 100)
+turtle.end_fill()
+
+#감괘 layer 2
+move(169.33, 50.85)
+turtle.color("white")
+turtle.begin_fill()
+square(8.33, 101)
+turtle.end_fill()
+
+#감괘 layer 3
+move(190.12, 64.71)
+turtle.begin_fill()
+square(8.33, 101)
+turtle.end_fill()
+
+#감괘 layer 4
+turtle.pensize(8.3)
+move(225, 150)
+turtle.goto(100, 66.66)
+
+#감괘 layer 5
+turtle.pensize(1)
+turtle.color("black")
+move(176.26, 55.47)
+turtle.begin_fill()
+square(16, 100)
+turtle.end_fill()
+
+
 
 
 time.sleep(3)
